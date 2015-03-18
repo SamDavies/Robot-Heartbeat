5b5dcde44a484e89c7dc84d661c9b6253730b213
Sam Davies <samdavies@live.co.uk>
2015-03-18 15:45:35 +0000

    fixed model import

--- app/database.py
+++ app/database.py
@@ -17,6 +17,7 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
+    import app.model
 
     # drop all to make a fresh database for testing
     Base.metadata.drop_all(bind=engine)
