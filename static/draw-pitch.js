a3f146fbfb8880b76e580fabfebe6cc25921d1a1
Sam Davies <samdavies@live.co.uk>
2015-03-28 17:15:04 +0000

    added diagram

---
+++ static/draw-pitch.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sam Davies on 28/03/15.
+ */
+
+window.onload = function() {
+    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
+
+    var circle = paper.circle(100, 100, 80);
+
+    var rectangle = paper.rect(200, 200, 250, 100);
+}
